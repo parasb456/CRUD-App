85ab73de8c7c10fbda22b40767f36e19ef09c22b
parasb456 <bparas606@gmail.com>
2024-06-08 21:33:25 +0530

    adding EKS configuration files

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -5,6 +5,8 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
 
+  cluster_endpoint_public_access = true
+
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
